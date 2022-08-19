68ac8c0e7f58fa55c1590715bf839b0487a885f7
Mamun Ahmed <mamunahmedusa@gmail.com>
2022-08-19 17:51:25 +0600

    This is a simple cart app develop with redux

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import Home from "./components/Home";
+import Main from "./components/Main";
+import { Provider } from "react-redux";
+import Store from "./components/Redux/Store";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={Store}>
+      <div className="bg-gray-50 h-full md:h-screen">
+        <Home />
+        <Main />
+      </div>
+    </Provider>
   );
 }
 
